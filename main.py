61bbae3bcda736161d77049f3b80b911737f01a7
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-29 21:51:00 +0900

    Make two variable to assign boolean type and int for logic this program

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ master = Tk()
 master.iconbitmap("juliao-martins.ico")
 master.title("Tic Tac Toe")
 
+clicked = True
+counts = 0
+
 def on_click(b):
     pass
 
-b1 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b1))
-b2 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b2))
-b3 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b3))
+b1 = Button(master, text="", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b1))
+b2 = Button(master, text="", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b2))
+b3 = Button(master, text="", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b3))
 
-b4 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b4))
-b5 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b5))
-b6 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b6))
+b4 = Button(master, text="", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b4))
+b5 = Button(master, text="", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b5))
+b6 = Button(master, text="", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b6))
 
-b7 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b7))
-b8 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b8))
-b9 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b9))
+b7 = Button(master, text="", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b7))
+b8 = Button(master, text="", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b8))
+b9 = Button(master, text="", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2, command=lambda: on_click(b9))
 
 
 
