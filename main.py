cd1f1299cd01f0f0c3338f9f6fd21731667d75bd
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-29 21:44:42 +0900

    Adding button classes as part of this game

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ master.iconbitmap("juliao-martins.ico")
 master.title("Tic Tac Toe")
 
 
+b1 = Button(master, text="X", fg="white", bg="black", font=("Apercu Mono",40), width=4, height=2)
+
+b1.grid(row=0, column=0)
 
 master.mainloop()
